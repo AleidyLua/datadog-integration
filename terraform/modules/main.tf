5d70c8be2b27e7532e0bd93b9679f20a516b70f8
aleidy97 <aleidy@iliosllc.com>
2024-11-26 09:49:39 -0800

    modify metric

--- terraform/modules/main.tf
+++ terraform/modules/main.tf
@@ -38,7 +38,7 @@ resource "datadog_monitor" "app_alerts" {
 
   name     = "App Alert for ${each.value}"
   type     = "metric alert"
-  query    = "avg(last_5m):avg:${each.value}{elasticbeanstalk_environment-name:${var.environment}} > 80"
+  query    = "avg(last_1h):avg:${each.value}{elasticbeanstalk_environment-name:${var.environment}} > 80"
   monitor_thresholds {
     critical = 80
   }
